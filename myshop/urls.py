aebabc63a2055731a7bcaecf1374ea699dcaf141
amirmm2 <amirhoseinmahm2002@gmail.com>
2025-10-16 20:40:34 +0330

    Update urls.py

--- myshop/urls.py
+++ myshop/urls.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.models import User  # 👈 اینو اضافه کن
+
+# 👇 این قسمت رو اضافه کن (بالای urlpatterns)
+if not User.objects.filter(username="Admin").exists():
+    User.objects.create_superuser("Amir", "amirhoseinmahm2002@gmail.com", "A09020039798a")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('cart/', include('cart.urls')),
-    path('payment/', include('payment.urls')),  # 👈 include کردن urls اپ payment
+    path('payment/', include('payment.urls')),
 ]
 
 if settings.DEBUG:
